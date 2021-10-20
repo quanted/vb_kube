62879f583eb8d235aba7670d893bd543c1d85781
Deron Smith <deron.brock@gmail.com>
2021-10-20 12:41:49 -0500

    reorg of kube repo

--- win-start.sh
+++ win-start.sh
@@ -11,11 +11,11 @@ ECHO "Mounting app_data directory from: $data_path"
 start "Mounting vb_django" minikube mount $django_path:/host/vb_django
 start "Mounting data_path" minikube mount $data_path:/host/app_data
 start "minikube dashboard" minikube dashboard
-timeout /T 30
+timeout 30.0
 ECHO "Applying WVB kubernetes elements"
-kubectl apply -f vb-postgresql.yml
-kubectl apply -f vb-dask.yml
-kubectl apply -f vb-django.yml
+kubectl apply -f k8s/vb-postgresql.yml
+kubectl apply -f k8s/vb-dask.yml
+kubectl apply -f k8s/vb-django.yml
 #start "minikube url" minikube service --url vb-django
 start "minikube tunnel" minikube tunnel
 ECHO "Completed kuberenetes startup of WVB"
